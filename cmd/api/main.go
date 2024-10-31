3999a1ee70cf3d59f9d8f601c863024827ba5840
Sudarsh1010 <sudarshjain1010@gmail.com>
2024-11-01 02:43:08 +0530

    add session helpers
    validate session, update session and generate session id

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -16,7 +16,11 @@ import (
 
 func gracefulShutdown(fiberServer *server.FiberServer, done chan bool) {
 	// Create context that listens for the interrupt signal from the OS.
-	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	ctx, stop := signal.NotifyContext(
+		context.Background(),
+		syscall.SIGINT,
+		syscall.SIGTERM,
+	)
 	defer stop()
 
 	// Listen for the interrupt signal.
@@ -26,8 +30,12 @@ func gracefulShutdown(fiberServer *server.FiberServer, done chan bool) {
 
 	// The context is used to inform the server it has 5 seconds to finish
 	// the request it is currently handling
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(
+		context.Background(),
+		5*time.Second,
+	)
 	defer cancel()
+
 	if err := fiberServer.ShutdownWithContext(ctx); err != nil {
 		log.Printf("Server forced to shutdown with error: %v", err)
 	}
